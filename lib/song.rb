3cff2110128488d819f5ec23bea77a12f75f9215
Shayla White <shayladwhite1@gmail.com>
2019-06-09 04:06:24 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,13 +1,55 @@
 class Song
   attr_accessor :name, :artist_name
   @@all = []
+  
+
+  def self.create(name = nil, artist_name = nil)
+   song = self.new 
+   song.name = name 
+   song.artist_name = artist_name
+      @@all << song
+      song
+  end  
 
   def self.all
     @@all
   end
+  
+  def self.new_by_name(name) 
+    song=self.new
+    song.name=name
+    song.save
+    song
+  end
 
-  def save
+ def self.create_by_name(name)
+    song=self.new
+    song.name=name
+    song.save
+    song
+  end
+ def self.find_by_name(name)
+    self.all.find{|song| song.name==name}
+  end
+
+def self.find_or_create_by_name(name) 
+    if self.find_by_name(name) == nil
+      self.create_by_name(name)
+    else
+      self.find_by_name(name)
+    end
+  end
+
+  def self.alphabetical
+    self.all.sort_by{|song| song.name}
+  end
+
+def self.new_from_filename(name)
+  song.all.chomp{|song| }
+end
+def save
     self.class.all << self
   end
 
 end
+
